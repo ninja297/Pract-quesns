32e6a565257fdc32bb74bffe7f6516c53ca50b85
Saloni Kushwaha <ninjaxoninja297@gmail.com>
2025-06-08 16:16:48 +0530

    Create valid_sudoku.cpp

---
+++ valid_sudoku.cpp
@@ -0,0 +1,29 @@
+//36. Valid sudoku 
+// couldn't solve - MARK FOR REV 
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> seen;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    string rowKey = string(1, c) + " in row " + to_string(i);
+                    string colKey = string(1, c) + " in col " + to_string(j);
+                    string boxKey = string(1, c) + " in box " + to_string(i / 3) + "-" + to_string(j / 3);
+                    
+                    if (seen.count(rowKey) || seen.count(colKey) || seen.count(boxKey)) {
+                        return false;
+                    }
+
+                    seen.insert(rowKey);
+                    seen.insert(colKey);
+                    seen.insert(boxKey);
+                }
+            }
+        }
+
+        return true;
+    }
+};
